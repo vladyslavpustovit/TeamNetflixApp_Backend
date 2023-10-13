53624db98b36fb66a75828d6721e185ce1d78935
vladyslavpustovit <Vladyslav.Pustovit@student.intecbrussel.be>
2023-10-13 06:56:11 +0200

    Dockerfile adjustment

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,30 @@
 # Node Version
-FROM node:18
+FROM node:lts-alpine
+
+WORKDIR /app
+
+RUN apk update && apk add --no-cache nmap && \
+    echo @edge https://dl-cdn.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories && \
+    echo @edge https://dl-cdn.alpinelinux.org/alpine/edge/main >> /etc/apk/repositories && \
+    apk update && \
+    apk add --no-cache \
+      chromium \
+      harfbuzz \
+      "freetype>2.8" \
+      ttf-freefont \
+      nss
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
 
 # Copy package.json and package-lock.json to the working directory
 COPY package*.json ./
 
-# Install Node.js dependencies
-RUN npm install
 
 # Copy the rest of the application code to the working directory
 COPY . .
+
+# Install Node.js dependencies
+RUN npm install
 
 # Expose a port
 EXPOSE 8181
--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -69,11 +69,21 @@ const scrapMovieTeaser = async (req, res) => {
     const { imdbId } = req.params;
 
     try {
-        const browser = await puppeteer.launch({ headless: "new" });
+        const browser = await puppeteer.launch({
+            args: [
+                "--no-sandbox",
+                "--disable-gpu"
+            ],
+            headless: "new",
+            executablePath: "/usr/bin/chromium-browser"
+        });
         const page = await browser.newPage();
+        console.log('Browser and page created successfully');
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36');
+        console.log('User agent set successfully');
 
         await page.goto(`https://www.imdb.com/title/${imdbId}/`);
+        console.log('Navigated to IMDb page');
 
         // Function to wait for and find an element
         const findElement = async (selector) => {
@@ -110,6 +120,7 @@ const scrapMovieTeaser = async (req, res) => {
         }
 
         await browser.close();
+        console.log('Browser closed successfully');
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
